df88ad9fa064caa950d3748d982d0c8a371f0351
tair18oz <tairoz@hilma.tech>
2025-01-28 10:35:41 +0200

    setup supabase

--- personal-assistant/src/Components/UserInput.tsx
+++ personal-assistant/src/Components/UserInput.tsx
@@ -1,10 +1,28 @@
 import React from "react";
 import { Messages } from "../App";
-
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 interface UserInputProps {
     setUserQuestion: React.Dispatch<React.SetStateAction<string>>;
     setMessages: React.Dispatch<React.SetStateAction<Messages>>;
     userQuestion: string;
+}
+
+try {
+    const result = await fetch("/info.txt");
+    console.log("result: ", result);
+    const text = await result.text();
+    console.log("text: ", text);
+
+    const splitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 350,
+        separators: ["\n\n", "\n", " ", ""],
+        chunkOverlap: 100,
+    });
+
+    const output = await splitter.createDocuments([text]);
+    console.log("output:", output);
+} catch (err) {
+    console.log(err);
 }
 
 const UserInput = ({ setUserQuestion, setMessages, userQuestion }: UserInputProps) => {
