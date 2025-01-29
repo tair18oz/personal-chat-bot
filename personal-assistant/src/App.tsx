73f7ae97c8673ba92254db7dae86387c4f682a78
tair18oz <tairoz@hilma.tech>
2025-01-29 11:14:12 +0200

    retriever blabla

--- personal-assistant/src/App.tsx
+++ personal-assistant/src/App.tsx
@@ -2,15 +2,14 @@ import UserInput from "./Components/UserInput";
 import AiMessage from "./Components/AiMessage";
 import UserMessage from "./Components/UserMessage";
 import { useState } from "react";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { createClient } from "@supabase/supabase-js";
-import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
-import { OpenAIEmbeddings } from "@langchain/openai";
 import "./App.css";
-import * as dotenv from "dotenv";
-import "./App.css";
+// import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+// import { createClient } from "@supabase/supabase-js";
+// import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
+// import { OpenAIEmbeddings } from "@langchain/openai";
+// import * as dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
 type message = {
     role: string;
@@ -18,39 +17,33 @@ type message = {
 };
 export type Messages = message[];
 
-try {
-    const result = await fetch("../public/info.txt");
-    console.log("result: ", result);
-    const text = await result.text();
-    console.log("text: ", text);
+// try {
+//     const result = await fetch("../public/info.txt");
+//     const text = await result.text();
 
-    const splitter = new RecursiveCharacterTextSplitter({
-        chunkSize: 350,
-        separators: ["\n\n", "\n", " ", ""],
-        chunkOverlap: 100,
-    });
+//     const splitter = new RecursiveCharacterTextSplitter({
+//         chunkSize: 350,
+//         separators: ["\n\n", "\n", " ", ""],
+//         chunkOverlap: 100,
+//     });
 
-    const output = await splitter.createDocuments([text]);
-    console.log("output:", output);
-    const embeddings = new OpenAIEmbeddings({
-        apiKey: import.meta.env.VITE_OPENAI_API_KEY,
-        model: "text-embedding-3-small",
-    });
+//     const output = await splitter.createDocuments([text]);
+//     const embeddings = new OpenAIEmbeddings({
+//         apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+//         model: "text-embedding-3-small",
+//     });
 
-    const supabaseClient = createClient(import.meta.env.VITE_SUPABASE_URL_TAIR, import.meta.env.VITE_SUPABASE_API_KEY_TAIR);
-    console.log("Supabase URL:", import.meta.env.VITE_SUPABASE_URL_TAIR);
-    console.log("Supabase Private Key:", import.meta.env.VITE_SUPABASE_API_KEY_TAIR);
+//     const supabaseClient = createClient(import.meta.env.VITE_SUPABASE_URL_TAIR, import.meta.env.VITE_SUPABASE_API_KEY_TAIR);
+//     const vectorStore = new SupabaseVectorStore(embeddings, {
+//         client: supabaseClient,
+//         tableName: "detailes",
+//         queryName: "match_detailes",
+//     });
 
-    const vectorStore = new SupabaseVectorStore(embeddings, {
-        client: supabaseClient,
-        tableName: "detailes",
-        queryName: "match_detailes",
-    });
-
-    await vectorStore.addDocuments(output);
-} catch (err) {
-    console.log(err);
-}
+//     await vectorStore.addDocuments(output);
+// } catch (err) {
+//     console.log(err);
+// }
 
 export default function App() {
     const [userQuestion, setUserQuestion] = useState<string>("");
--- personal-assistant/src/Components/UserInput.tsx
+++ personal-assistant/src/Components/UserInput.tsx
@@ -1,45 +1,58 @@
 import React from "react";
 import { Messages } from "../App";
+import { ChatOpenAI } from "@langchain/openai";
+import { PromptTemplate } from "@langchain/core/prompts";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import { RunnableSequence } from "@langchain/core/runnables";
+import { retriever } from "/utils/retriever";
+import { combineDocuments } from "/utils/combineDocuments";
+
 interface UserInputProps {
-  setUserQuestion: React.Dispatch<React.SetStateAction<string>>;
-  setMessages: React.Dispatch<React.SetStateAction<Messages>>;
-  userQuestion: string;
+    setUserQuestion: React.Dispatch<React.SetStateAction<string>>;
+    setMessages: React.Dispatch<React.SetStateAction<Messages>>;
+    userQuestion: string;
 }
 
-const UserInput = ({
-  setUserQuestion,
-  setMessages,
-  userQuestion,
-}: UserInputProps) => {
-  const handleAsk = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    e.preventDefault();
-    setUserQuestion("");
-    setMessages((prev: Messages) => [
-      ...prev,
-      {
-        role: "user",
-        content: userQuestion,
-      },
-    ]);
-  };
+const openAIApiKey = process.env.OPENAI_API_KEY;
+const llm = new ChatOpenAI({ openAIApiKey });
 
-  return (
-    <>
-      <form>
-        <br />
-        <input
-          type="text"
-          id="UserMessage"
-          name="UserMessage"
-          value={userQuestion}
-          onChange={(e) => setUserQuestion(e.target.value)}
-        />
-        <br />
-        <br />
-        <button onClick={(e) => handleAsk(e)}>ask</button>
-        <br />
-      </form>
-    </>
-  );
+const UserInput = ({ setUserQuestion, setMessages, userQuestion }: UserInputProps) => {
+    const standaloneQuestionTemplate = "Given a question, convert it to a standalone question. question: {question} standalone question:";
+    const standaloneQuestionPrompt = PromptTemplate.fromTemplate(standaloneQuestionTemplate);
+
+    const answerTemplate = `You are a helpful and enthusiastic support bot who can answer a given question about the user based on the context provided from supabase. Try to find the answer in the context. If you really don't know the answer, say "I'm sorry, I don't know the answer to that." And direct the questioner to email blablabla@gmail.com. Don't try to make up an answer. Always speak as if you were chatting to a friend.
+    context: {context}
+    question: {question}
+    answer: `;
+    const answerPrompt = PromptTemplate.fromTemplate(answerTemplate);
+
+    const standaloneQuestionChain = standaloneQuestionPrompt.pipe(llm).pipe(new StringOutputParser());
+
+    const retrieverChain = RunnableSequence.from([(prevResult) => prevResult.standalone_question, retriever, combineDocuments]);
+
+    const handleAsk = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+        setUserQuestion("");
+        setMessages((prev: Messages) => [
+            ...prev,
+            {
+                role: "user",
+                content: userQuestion,
+            },
+        ]);
+    };
+
+    return (
+        <>
+            <form>
+                <br />
+                <input type="text" id="UserMessage" name="UserMessage" value={userQuestion} onChange={(e) => setUserQuestion(e.target.value)} />
+                <br />
+                <br />
+                <button onClick={(e) => handleAsk(e)}>ask</button>
+                <br />
+            </form>
+        </>
+    );
 };
 export default UserInput;
