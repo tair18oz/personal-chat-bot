88710d0c144b4652fff9eb60e662273856f299b4
chen-orenstein <chenorenstein@hilma.tech>
2025-01-29 11:15:09 +0200

    something wrong

--- personal-assistant/src/Components/UserInput.tsx
+++ personal-assistant/src/Components/UserInput.tsx
@@ -1,16 +1,33 @@
 import React from "react";
 import { Messages } from "../App";
+import { ChatOpenAI } from "@langchain/openai";
+import { PromptTemplate } from "@langchain/core/prompts";
+
 interface UserInputProps {
   setUserQuestion: React.Dispatch<React.SetStateAction<string>>;
   setMessages: React.Dispatch<React.SetStateAction<Messages>>;
   userQuestion: string;
 }
 
+const openAIApiKey = process.env.OPENAI_API_KEY;
+const llm = new ChatOpenAI({ openAIApiKey });
+
 const UserInput = ({
   setUserQuestion,
   setMessages,
   userQuestion,
 }: UserInputProps) => {
+  const standaloneQuestionTemplate =
+    "Given a question, convert it to a standalone question. question: {question} standalone question:";
+  const standaloneQuestionPrompt = PromptTemplate.fromTemplate(
+    standaloneQuestionTemplate
+  );
+  const answerTemplate = `You are a helpful and enthusiastic support bot who can answer a given question about Scrimba based on the context provided. Try to find the answer in the context. If you really don't know the answer, say "I'm sorry, I don't know the answer to that." And direct the questioner to email help@scrimba.com. Don't try to make up an answer. Always speak as if you were chatting to a friend.
+context: {context}
+question: {question}
+answer: `;
+  const answerPrompt = PromptTemplate.fromTemplate(answerTemplate);
+
   const handleAsk = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
     setUserQuestion("");
